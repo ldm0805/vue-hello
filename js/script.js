2ef30a835b688927dd37045d830780aa1a0328a2
Leonardo Di Maggio <leonardodimaggio94@gmail.com>
2022-12-16 14:08:17 +0100

    bons del bonus completo

--- js/script.js
+++ js/script.js
@@ -5,8 +5,22 @@
  createApp({ 
      data(){ 
          return{
-             message: 'Ciao Vujs',
-             link_img: 'https://unsplash.it/300/300?image=29'
+             name: null,
+             surname: null,
+             margin_top: 'mt-4' ,
+             custom_style: 'text-danger mb-5' ,
+             message: 'Booleaner quando escono dalla valle della disperazione:',
+             message_two: 'Scrivi il tuo nome e cognome se sei appena uscito dalla valle di lacrime:',
+             link_img: 'https://static.wikia.nocookie.net/disney/images/7/7e/Hercules-br-disneyscreencaps.com-9797.jpg/revision/latest/scale-to-width-down/250?cb=20140613063303'
          }
-     }
+         
+     },
+     methods:{
+        greetings: function(){   //oppure greetings(){}
+            this.name
+            this.surname;
+            this.name_surname = this.name + '' + this.surname;
+           console.log(this.surname) // senza this non funziona
+        },
+    }
  }).mount('#app')
