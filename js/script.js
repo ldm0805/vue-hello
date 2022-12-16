1b5f3a8eec1795419d0d9151eb6a6bf631e138bb
Leonardo Di Maggio <leonardodimaggio94@gmail.com>
2022-12-16 14:10:40 +0100

    aggiunta commenti

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@
              surname: null,
              margin_top: 'mt-4' ,
              custom_style: 'text-danger mb-5' ,
+             flex_classes: 'd-flex flex-column justify-content-center align-items-center',
              message: 'Booleaner quando escono dalla valle della disperazione:',
              message_two: 'Scrivi il tuo nome e cognome se sei appena uscito dalla valle di lacrime:',
              link_img: 'https://static.wikia.nocookie.net/disney/images/7/7e/Hercules-br-disneyscreencaps.com-9797.jpg/revision/latest/scale-to-width-down/250?cb=20140613063303'
