3777a9f4ad1858fc656decf58252291f6a1b202c
Leonardo Di Maggio <leonardodimaggio94@gmail.com>
2022-12-16 13:43:14 +0100

    esercizio con bonus fatto

--- js/script.js
+++ js/script.js
@@ -5,7 +5,8 @@
  createApp({ 
      data(){ 
          return{
-             message: 'Scrivi qui il tuo contenuto'
+             message: 'Ciao Vujs',
+             link_img: 'https://unsplash.it/300/300?image=29'
          }
      }
  }).mount('#app')
